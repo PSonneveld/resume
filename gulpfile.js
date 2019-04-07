ec258454e3671d90dad310a04baae2f7d15be24e
Patrick Sonneveld <p.sonneveld@goesenroos.nl>
2019-04-07 21:45:55 +0200

    Update for content of resume

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
-var gulp   = require('gulp');
-var deploy = require('gulp-gh-pages');
+const gulp = require('gulp');
+const deploy = require('gulp-gh-pages');
+const exec = require('child_process').exec;
+const rimraf = require('rimraf');
 
-gulp.task('deploy', function () {
-  return gulp.src("./docs/**/*")
+gulp.task('clean', (cb) => {
+  rimraf('./public', cb);
+});
+
+gulp.task('build', gulp.series('clean', (cb) => {
+  exec('hugo', function (err, stdout, stderr) {
+    console.log('\n-------- Hugo output--------\n');
+    console.log(stdout);
+    console.log('----------------------------\n');
+
+    cb(err);
+  });
+}));
+
+gulp.task('deploy', gulp.series('build', () => {
+  return gulp.src("./public/**/*")
     .pipe(deploy({
       remoteUrl: "https://github.com/PSonneveld/resume.git",
       branch: "master"
     }))
-});
+}));
